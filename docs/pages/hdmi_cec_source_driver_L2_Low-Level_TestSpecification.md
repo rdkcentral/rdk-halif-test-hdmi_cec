c4c3ac1d132385e68f860bb1c874c1da43916e63
saniya sebastian <Saniya_Sebastian2@comcast.com>
2024-06-19 10:04:34 +0000

    L2 Tests for HDMI CEC source Generate L2 tests for HDMI CEC source

---
+++ docs/pages/hdmi_cec_source_driver_L2_Low-Level_TestSpecification.md
@@ -0,0 +1,74 @@
+# HDMI CEC DRIVER L2 Low Level Test Specification and Procedure Documentation
+
+## Table of Contents
+
+- [HDMI CEC DRIVER L2 Low Level Test Specification and Procedure Documentation](#hdmi-cec-driver-l2-low-level-test-specification-and-procedure-documentation)
+
+  - [Table of Contents](#table-of-contents)
+  - [Overview](#overview)
+    - [Acronyms, Terms and Abbreviations](#acronyms-terms-and-abbreviations)
+    - [Definitions](#definitions)
+    - [References](#references)
+  - [Level 2 Test Procedure](#level-2-test-procedure)
+
+## Overview
+
+This document describes the level 2 testing suite for the HDMI CEC DRIVER module.
+
+### Acronyms, Terms and Abbreviations
+
+- `HAL` \- Hardware Abstraction Layer, may include some common components
+- `UT`  \- Unit Test(s)
+- `OEM`  \- Original Equipment Manufacture
+- `SoC`  \- System on a Chip
+
+### Definitions
+
+  - `ut-core` \- Common Testing Framework <https://github.com/rdkcentral/ut-core>, which wraps a open-source framework that can be expanded to the requirements for future framework.
+
+### References
+- `High Level Test Specification` - [hdmi_cec_source_tests.md](hdmi_cec_source_tests.md)
+
+## Level 2 Test Procedure
+
+The following functions are expecting to test the module operates correctly.
+
+### Test 1
+
+|Title|Details|
+|--|--|
+|Function Name|`test_l2_hdmi_cec_driver_ValidateLogicalAddressUnavailability_source`|
+|Description|Trying to get a logical address discovered during CEC open and validate the return value when the `DUT` is not connected to a Sink device. It should return HDMI_CEC_IO_LOGICALADDRESS_UNAVAILABLE.|
+|Test Group|Module : 02|
+|Test Case ID|001|
+|Priority|High|
+
+**Pre-Conditions :**
+None
+
+**Dependencies :**
+None
+
+**User Interaction :**
+If user chose to run the test in interactive mode, then the test case has to be selected via console.
+
+#### Test Procedure :
+
+| Variation / Steps | Description | Test Data | Expected Result | Notes|
+| -- | --------- | ---------- | -------------- | ----- |
+| 01 | Invoke HdmiCecOpen with a valid handle when the `DUT` is not connected to a Sink device | handle = valid handle | HDMI_CEC_IO_LOGICALADDRESS_UNAVAILABLE | Should be successful |
+| 02 | If the status is not HDMI_CEC_IO_SUCCESS, invoke HdmiCecClose with the handle | handle = valid handle | HDMI_CEC_IO_SUCCESS | Should be successful |
+
+
+```mermaid
+graph TB
+    Step1[Call HdmiCecOpen] -->|Success| Step2[Check returned status]
+    Step1 -->|Failure| TestFail1[Test Case Failed: HdmiCecOpen failed]
+    Step2 -->|Status is <br> HDMI_CEC_IO_LOGICALADDRESS_UNAVAILABLE| Step4[TestCase Success]
+    Step2 -->|Status is not <br> HDMI_CEC_IO_LOGICALADDRESS_UNAVAILABLE| TestFail2[Test Case Failed]
+    TestFail2 -->|Status is HDMI_CEC_IO_SUCCESS| Step3[Call HdmiCecClose]
+    Step3 -->|Success| Step5[HdmiCecClose success]
+    Step3 -->|Failure| TestFail3[Test Case Failed: HdmiCecClose failed]
+```
+
+
---
+++ src/test_l2_hdmi_cec_source_driver.c
@@ -0,0 +1,99 @@
+/*
+* If not stated otherwise in this file or this component's LICENSE file the
+* following copyright and licenses apply:*
+* Copyright 2024 RDK Management
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+/**
+* @file test_l2_hdmi_cec_source_driver.c
+* @page hdmi_cec_driver Level 2 Tests
+*
+* ## Module's Role
+* This module includes Level 2 functional tests (success and failure scenarios).
+* This is to ensure that the hdmi_cec_driver APIs meet the requirements across all vendors.
+*
+* **Pre-Conditions:**  None@n
+* **Dependencies:** None@n
+*
+* Ref to API Definition specification documentation : [hdmi-cec_halSpec.md](../../docs/pages/hdmi-cec_halSpec.md)
+*/
+
+#include <ut.h>
+#include <ut_log.h>
+#include "hdmi_cec_driver.h"
+
+static int gTestGroup = 2;
+static int gTestID = 1;
+
+/**
+* @brief This test validates the unavailability of logical address in HDMI CEC driver
+*
+* This test case is designed to validate the scenario when the logical address is unavailable in the HDMI CEC driver. The test invokes the HdmiCecOpen function and checks if the returned status is HDMI_CEC_IO_LOGICALADDRESS_UNAVAILABLE. If the status is not HDMI_CEC_IO_SUCCESS, it invokes the HdmiCecClose function and checks if the returned status is HDMI_CEC_IO_SUCCESS.
+*
+* **Test Group ID:** 02@n
+* **Test Case ID:** 001@n
+*
+* **Test Procedure:**
+* Refer to UT specification documentation [hdmi_cec_driver_source_L2_Low-Level_TestSpecification.md](../../docs/pages/hdmi_cec_source_driver_L2_Low-Level_TestSpecification.md)
+*/
+
+void test_l2_hdmi_cec_driver_ValidateLogicalAddressUnavailability_source(void)
+{
+    gTestID = 1;
+    UT_LOG_INFO("In %s [%02d%03d]\n", __FUNCTION__, gTestGroup, gTestID);
+
+    int handle;
+    HDMI_CEC_STATUS status;
+
+    UT_LOG_DEBUG("Invoking HdmiCecOpen with valid handle");
+    status = HdmiCecOpen(&handle);
+    UT_LOG_DEBUG("Returned status: %d and handle: %d", status, handle);
+    // Return value should be HDMI_CEC_IO_LOGICALADDRESS_UNAVAILABLE, when the DUT is not connected to a Sink device.
+    UT_ASSERT_EQUAL(status, HDMI_CEC_IO_LOGICALADDRESS_UNAVAILABLE);
+
+    if (status == HDMI_CEC_IO_SUCCESS)
+    {
+        UT_LOG_DEBUG("Invoking HdmiCecClose with handle: %d", handle);
+        HDMI_CEC_STATUS close_status = HdmiCecClose(handle);
+        UT_LOG_DEBUG("Returned status: %d", close_status);
+
+        UT_ASSERT_EQUAL_FATAL(close_status, HDMI_CEC_IO_SUCCESS);
+    }
+
+    UT_LOG_INFO("Out %s\n", __FUNCTION__);
+}
+
+static UT_test_suite_t * pSuite = NULL;
+
+/**
+ * @brief Register the main tests for this module
+ *
+ * @return int - 0 on success, otherwise failure
+ */
+
+int test_hdmi_cec_driver_l2_register(void)
+{
+    // Create the test suite
+    pSuite = UT_add_suite("[L2 hdmi_cec_driver]", NULL, NULL);
+    if (pSuite == NULL)
+    {
+        return -1;
+    }
+    // List of test function names and strings
+
+    UT_add_test( pSuite, "l2_hdmi_cec_driver_ValidateLogicalAddressUnavailability_source", test_l2_hdmi_cec_driver_ValidateLogicalAddressUnavailability_source);
+
+    return 0;
+}
